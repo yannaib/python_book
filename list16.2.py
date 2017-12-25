1bbd41fb1748b9ed7328c8b85251905b2f4966ce
yannaib <34744977+yannaib@users.noreply.github.com>
2017-12-25 13:47:18 +0200

    changes 22.12.2017

--- list16.2.py
+++ list16.2.py
@@ -1,10 +1,27 @@
-import pygame, sys
+import pygame, sys, random
+white=255
 pygame.init ()
 screen=pygame.display.set_mode ([640,480])
-screen.fill([63,63,63])
-pygame.draw.circle(screen,[0,191,0],[320,240],30,0)
+screen.fill([white,white,white])
+for i in range (1):
+	width=random.randint (0,250)
+	height=random.randint (0,100)
+	top=random.randint (0,400)
+	left=random.randint (0,500)
+	red=random.randint (0, 255)
+	green=random.randint (0, 255)
+	blue=random.randint (0, 255)
+	pygame.draw.rect (screen, [red, green, blue], [left, top, width, height], 3)
 pygame.display.flip()
+'''
+pygame.draw.circle(screen,[0,191,0], [ 320, 240], 30)
+rect_color=[255,255,255]
+rect_list=(250,150,300,200)
+pygame.draw.rect(screen, rect_color, rect_list, 0)
+pygame.display.flip()
+'''
 while (True):
     for event in pygame.event.get ():
         if event.type==pygame.QUIT:
-            sys.exit () 
+            sys.exit ()
+		#elif event.type==pygame. 
